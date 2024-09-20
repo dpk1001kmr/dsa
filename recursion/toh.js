de6ab05480db0d7d942b6984c8f78eae6fdc8abc
dpk1001kmr <deepak8364kumar@gmail.com>
2024-09-20 07:28:15 +0530

    tower of hanoi recursion problem solved

--- recursion/toh.js
+++ recursion/toh.js
@@ -2,6 +2,6 @@ export function toh(n, source, helper, destination) {
   if (n === 0) return;
 
   toh(n - 1, source, destination, helper);
-  console.log(`${n} => [${source} - ${destination}]`);
+  console.log(`[${n}] => [${source} - ${destination}]`);
   toh(n - 1, helper, source, destination);
 }
