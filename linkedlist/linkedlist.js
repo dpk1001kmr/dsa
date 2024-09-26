abb9cb3a43847c0f86d55c4b5529a446caa3bf2e
dpk1001kmr <deepak8364kumar@gmail.com>
2024-09-26 16:40:24 +0530

    linkedlist data structure

--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -41,7 +41,6 @@ export class LinkedList {
   removeFirst() {
     if (this.size === 0) {
       console.log("[[LinkedList is Empty]]");
-      return;
     } else if (this.size === 1) {
       this.head = null;
       this.tail = null;
@@ -97,6 +96,60 @@ export class LinkedList {
       temp.next = this.head;
       this.head = temp;
       this.size++;
+    }
+  }
+
+  addAt(index, data) {
+    if (index < 0 || index > this.size) {
+      console.log("[[Invalid Arguments]]");
+    } else if (index === 0) {
+      this.addFirst(data);
+    } else if (index === this.size) {
+      this.addLast(data);
+    } else {
+      let node = new Node(data);
+      let temp = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        temp = temp.next;
+      }
+      node.next = temp.next;
+      temp.next = node;
+      this.size++;
+    }
+  }
+
+  removeLast() {
+    if (this.size === 0) {
+      console.log("[[LinkedList is Empty]]");
+    } else if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
+    } else {
+      let temp = this.head;
+      for (let i = 0; i < this.size - 2; i++) {
+        temp = temp.next;
+      }
+      this.tail = temp;
+      this.tail.next = null;
+      this.size--;
+    }
+  }
+
+  removeAt(index) {
+    if (index < 0 || index >= this.size) {
+      console.log("[[Inavlid Arguments]]");
+    } else if (index === 0) {
+      this.removeFirst();
+    } else if (index === this.size - 1) {
+      this.removeLast();
+    } else {
+      let temp = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        temp = temp.next;
+      }
+      temp.next = temp.next.next;
+      this.size--;
     }
   }
 }
